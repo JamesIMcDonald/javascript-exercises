ff53d3d2181ac3aaba5d72f01c9887f64c775a38
JamesIMcDonald <144733089+JamesIMcDonald@users.noreply.github.com>
2023-11-01 22:18:28 +0000

    finished the first 2 problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string,repeatnum) {
+    let word = ''
+    if (repeatnum < 0) {return 'ERROR'}
+        for (let i = 0; i < repeatnum; i++) {
+            word += string
+        }
+        return word
+    }
+
+
+
+// so i want it to take a string in this case hey and then add it to itself n number of times
 
 // Do not edit below this line
 module.exports = repeatString;
