13446176b66e6e3ef233d777ae675905521e4d36
JamesIMcDonald <144733089+JamesIMcDonald@users.noreply.github.com>
2023-11-01 23:34:43 +0000

    finished ex 3 and wrote out how i want to make a function to complete ex 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
-
+const reverseString = function reverse(string) {
+    let stringlength = string.length
+    let reversestring = ''
+    function extract(charnum) {
+        let character = string.substr(charnum,1)
+        return character
+    }
+    for (let i = stringlength-1; i >= 0 ; i--) {
+        reversestring += extract(i)
+    }
+    return reversestring
 };
+
+// how this could work 
+// get the length of the string -1 so that its correct for substring
+// then iteratively going backwards and substringing that number from it into a new string
+// then returning that string at the end
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removals) {
 
 };
 
+
+
+// so, we get given an array and something we need to remove
+// the rest function should be able to do this
+// 
+// it needs to see the array and the removals array
+// for each entry in removals array it needs to remove all copies in the actual array
+// this is nested loops - should be a few functions 
+// the main function should be a loop which tells the removal function to remove
+// so main goes through the removals array and calls the removals function which gets rid of all cases of an object in the array through some sort of counting and locating loop
 // Do not edit below this line
 module.exports = removeFromArray;
