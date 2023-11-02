85d29f827e0fc7f3362e58c3f543bf7addeceb15
James McDonald <James@MellcrestLtd.local>
2023-11-02 16:53:58 +0000

    finished ex 5,6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalNum = 0
+    function isNumber(input) {
+        if (typeof input === 'number') {
+            return true
+        }
+        else {return false}
+    }
+    if (isNumber(num1) === false || isNumber(num2) === false || num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+        else {
+        for (let i = Math.max(num1, num2); i >= Math.min(num1, num2); i--) {
+        finalNum += i
+    }
+    return finalNum;
+    }
 };
+
+
+
+// so this function takes num1 and num2, collects all of the numbers in between them and then adds them all together
+// considering making a loop that counts down and then adds that to a separate variable which is the answer
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year === 'string') {return 'ERROR'}
+    if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    }
+    else if (year % 100 === 0) {
+        return false
+    }
+    else if (year % 4 === 0) {
+        return true
+    }
+    else {return false}
 };
+
+
+
+// so, leap year has to be divisible by 4
+// if its divisible by 100 its not a leap year HOwever this doesnt apply if it is also divisible by 400
+
 
 // Do not edit below this line
 module.exports = leapYears;
