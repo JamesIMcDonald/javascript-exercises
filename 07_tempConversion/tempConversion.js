988dfa0ff52c4a2f15c2b75110f212cc1dd734fb
James McDonald <James@MellcrestLtd.local>
2023-11-02 16:56:11 +0000

    finished 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let celsius = Math.round(((temp - 32)/1.8)*10)/10
+    return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let fahrenheit = Math.round((temp*1.8+32)*10)/10
+    return fahrenheit
 };
 
+
+// just a simple conversion and then a math.floor so that we have one decimal place
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
