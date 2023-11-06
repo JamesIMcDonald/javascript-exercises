23fbe2ddc0d41ce9fb8f4fb48cb285a176516f90
James McDonald <James@MellcrestLtd.local>
2023-11-06 15:20:56 +0000

    completed 8 and am working on 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let product = num1 + num2;
+  return product
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let product = num1 - num2;
+  return product
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let product = 0
+  numbers.forEach(number => {product += number} );
+  return product
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1
+  // let numbers = numbersArray.split (',')
+  numbers.forEach(number => {product *= parseFloat(number)})
+  return product
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+	let product = (num1 * (num1 * power + num1))
+  return product
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let product = 1
+	if (num1 === 0) {
+    return 1
+  }
+  else {
+    for (let i = num1; i > 0; i--) {
+    product * i
+    }
+    return product
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let workingString = string.replace(/\w|\d/gi, '').toLowerCase()
+    let stringLength = workingString.length
+    let valid = true
+    for (let i = 0; i < stringLength / 2; i++) {
+        if (workingString[i] !== workingString[stringLength - 1 - i]) {
+            valid = false
+            break
+        }
+    }
+    return valid
 };
 
+
+// so this function needs to take an input clean it up so that it is just a string of characters and then determine if that is a palindrome
+// for this we probably just want it to grab every letter
+// for palindrome detection: we could do something like make sure that the length is even or half it and chop the extra letter off if not
+// then just reverse the second order and make sure there is a strict equality.
 // Do not edit below this line
 module.exports = palindromes;
