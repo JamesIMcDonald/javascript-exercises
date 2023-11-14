1e3844b828c185b96dde3db251e5a23dd2be098d
James McDonald <James@MellcrestLtd.local>
2023-11-14 15:54:51 +0000

    went through the solution of 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
 const findTheOldest = function(arrayName) {
-let oldest = -1
+let oldest = -Infinity
 let oldestId = 0
 let oldestName = ''
 for (let i = 0; i <= arrayName.length-1; i++) {
@@ -7,19 +7,19 @@ for (let i = 0; i <= arrayName.length-1; i++) {
     // so this checks if the yearofdeath is falsy if it is it means the person is still alive
     if (!arrayName[i].yearOfDeath) {
         age = new Date().getFullYear() - arrayName[i].yearOfBirth
-    }
+        }
     else {
         age = arrayName[i].yearOfDeath - arrayName[i].yearOfBirth
-    }
+        }
 
     if (age > oldest) {
         oldest = age
         oldestId = i
         oldestName = arrayName[i].name
-}
+        }
 };
 return {oldestName}
 }
-
+// this doesn't work and I can't see why so I have gone through the solution to understand why it works
 // Do not edit below this line
 module.exports = findTheOldest;
--- 12_findTheOldest/solution/findTheOldest-solution.js
+++ 12_findTheOldest/solution/findTheOldest-solution.js
@@ -1,14 +1,17 @@
 const findTheOldest = function (array) {
   return array.reduce((oldest, currentPerson) => {
+    // so with a reduce they have oldest which is the value that gets carried in the loop and current person which is the current value
+    // oldest age is set here with the getAge so its known but can change when someone older is found
     const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
     const currentAge = getAge(
       currentPerson.yearOfBirth,
       currentPerson.yearOfDeath
     );
+    // here it is setting the accumulator (oldest) to the oldest person by using a ternary operator which is a super basic if statement
     return oldestAge < currentAge ? currentPerson : oldest;
   });
 };
-
+// so this gets their age as a single number and deals with when they arent dead yet
 const getAge = function (birth, death) {
   if (!death) {
     death = new Date().getFullYear();
